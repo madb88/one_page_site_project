2bd63cbb9123bbc76e98a8561ca117c66ba1c9fe
madb88 <kaminskiqba@gmail.com>
2016-08-16 18:52:20 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3c.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+	<title>Wspieramy Lukasza - Koncert</title>
+  <meta charset="utf-8">	
+  <!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+	<!-- WL CSS -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+  <!-- NAVBAR -->
+
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"><img style="max-width:100px; margin-top: -10px; height:40px" src="img/littledude1.png"></a>
+      </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#welcomeSection">Home</a></li>
+          <li><a href="#artist">Artyści</a></li>
+          <li><a href="#sponsor">Sponsorzy</a></li>
+          <li><a href="#tickets">Bilety</a></li>
+          <li><a href="#map">Adres</a></li>
+          <li><a href="#contact">Kontakt</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+
+    </div><!-- /.container-fluid -->
+  </nav>
+
+<!-- FIRST PANEL -->
+<header id="home" class="header">
+  <div class="center-content">
+    <h1>WSPIERAMY LUKASZA - KONCERT</h1>
+    <h3>Nowa Sól Pilsudskiego 61 HALA SPORTOWA ELEKTRYK</h3>
+    <h3>DATA</h3>
+    <div class="logo-img"></div>
+    <br>
+    <a href="#bilet" class="btn btn-default btn-lg">Kup bilet</a>
+  </div>
+</header>
+
+<!-- Welcome Section -->
+<section id="welcomeSection" class="welcomeSection">
+  <div class="container-fluid">
+    <div class="row text-center">  
+      <div class="col-lg-10 col-lg-offset-1">
+      <h2>WITAMY</h2>
+    </div>
+    <div class="col-lg-6 col-lg-offset-3">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique sapien a venenatis cursus. Phasellus pulvinar ultrices justo in vulputate. Suspendisse porttitor, turpis eget pharetra tempor, felis velit congue diam, pulvinar varius nulla tortor non ligula. Duis consequat elit in commodo semper. Sed vel arcu quis libero bibendum pulvinar eu in augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed nisl nulla, accumsan eget euismod viverra, malesuada sed mauris. Morbi ipsum massa, faucibus in erat et, volutpat cursus enim. In consequat dolor luctus fringilla pellentesque.
+Nam turpis enim, blandit eu sagittis sed, imperdiet nec erat. Cras faucibus dolor pellentesque sem laoreet tincidunt. Sed bibendum, turpis sed tincidunt ultrices, ex enim dictum lorem, at lacinia purus diam eget dolor. Morbi viverra consectetur sem, sed vehicula purus pretium vitae. Nullam quis lectus sit amet ipsum posuere lobortis elementum ac justo. Aliquam id ornare odio. Nulla pulvinar dolor sed leo iaculis iaculis. Donec iaculis tincidunt dui, eget bibendum leo accumsan nec. Nam luctus, diam at porta pellentesque, ante nisl posuere lorem, pellentesque faucibus ligula nulla vitae felis. Mauris et rhoncus turpis. Nulla maximus nunc quis lectus gravida sodales. Curabitur eu dui id sem dignissim elementum vel ut leo. Nunc pulvinar est quis nibh hendrerit, et semper sem cursus.
+pretium pulvinar eros pulvinar eleifend. Fusce blandit ornare urna quis pellentesque. Etiam eget sem dui. Mauris sit amet molestie diam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum vitae metus maximus, placerat libero at, fermentum nisl. In magna dui, vulputate sit amet velit sit amet, ultricies tincidunt nunc. Vivamus auctor nulla vel molestie volutpat. Vivamus commodo semper nisl, eget varius nibh pulvinar non. Suspendisse tempus non ipsum vitae finibus. Nam faucibus ligula sapien, ac ullamcorper orci iaculis suscipit. 
+</p>
+<br>
+    <a href="#bilet" class="btn btn-default btn-lg">Kup bilet</a>
+  </div>
+</div>
+    </div>
+  </div>  
+</section>
+
+
+<!-- ARTIST -->
+<section id="artist" class="artist">
+  <div class="container-fluid">
+    <div class="row text-center">
+      <div class="col-lg-10 col-lg-offset-1">
+        <h2>ARTYŚCI</h2>
+        <hr class="small">
+        <br>
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="artist-one">
+              <a href="#"><img class="featurette-image img-rounded img-responsive pull-left" src="img/malolatpezet.jpg"></a>
+              <span class="artist-text">
+                <strong>Pezet &amp Malolat</strong>
+              </span>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="artist-one">
+              <a href="#"><img class="featurette-image img-rounded img-responsive pull-left" src="img/malolatpezet.jpg"></a>
+              <span class="artist-text">
+                <strong>Pezet &amp Malolat</strong>
+              </span>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="artist-one">
+              <a href="#"><img class="featurette-image img-rounded img-responsive pull-left" src="img/malolatpezet.jpg"></a>
+              <span class="artist-text">
+                <strong>Pezet &amp Malolat</strong>
+              </span>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="artist-one">
+              <a href="#"><img class="featurette-image img-rounded img-responsive pull-left" src="img/malolatpezet.jpg"></a>
+              <span class="artist-text">
+                <strong>Pezet &amp Malolat</strong>
+              </span>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="artist-one">
+              <a href="#"><img class="featurette-image img-rounded img-responsive pull-left" src="img/malolatpezet.jpg"></a>
+              <span class="artist-text">
+                <strong>Pezet &amp Malolat</strong>
+              </span>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="artist-one">
+              <a href="#"><img class="featurette-image img-rounded img-responsive pull-left" src="img/malolatpezet.jpg"></a>
+              <span class="artist-text">
+                <strong>Pezet &amp Malolat</strong>
+              </span>
+            </div>
+          </div>
+        </div>
+        </div>
+    </div>
+  </div>
+</section>
+
+<!-- Sponsor -->
+<section id="sponsor" class="sponsor">
+  <div class="container-fluid">
+    <div class="row text-center">
+      <div class="col-lg-10 col-lg-offset-1">
+        <h2>SPONSORZY</h2>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <a href="#"><img class="featurette-image img-circle img-responsive pull-left sponsor-img" src="img/sponsor/cocacola.png"></a>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <a href="#"><img class="featurette-image img-circle img-responsive pull-left sponsor-img" src="img/sponsor/cocacola.png"></a>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <a href="#"><img class="featurette-image img-circle img-responsive pull-left sponsor-img" src="img/sponsor/cocacola.png"></a>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <a href="#"><img class="featurette-image img-circle img-responsive pull-left sponsor-img" src="img/sponsor/cocacola.png"></a>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <a href="#"><img class="featurette-image img-circle img-responsive pull-left sponsor-img" src="img/sponsor/cocacola.png"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- TICKETS -->
+<section id="tickets" class="tickets">
+  <div class="container-fluid">
+    <div class="row text-center">
+      <div class="col-lg-10 col-lg-offset-1">
+        <h2>BILETY</h2>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- MAP -->
+<section id="map" class="map">
+  <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2466.7585619317765!2d15.708729716261935!3d51.81057207968626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4706062d18b61e4b%3A0xe2df4832d05a4760!2sHala+sportowa+Centrum+Kszta%C5%82cenia+Zawodowego+i+Ustawicznego+%22ELEKTRYK%22!5e0!3m2!1spl!2spl!4v1471277749163"></iframe>
+</iframe>
+</section>
+
+<footer>
+  <div class="container-fluid">
+    <div class="row text-center">
+      <p>Developed by <a href="https://github.com/madb88" target="_blank">Jakub Kamiński</a> 2016</p>
+    </div>
+  </div>
+</footer>
+
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/app.js"></script>
+
+</body>
+</html>
